5f7a65279b033bfd10f879373b200df02c0bce64
danbihyun <sessak02@naver.com>
2025-09-09 14:50:12 +0900

    update

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,23 @@
+/*소스 정보
+ * 작성자: 현단비
+ * 작성일: 2025.09.09
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        int v1 = 15;
+        if (v1 > 10) {
+            int v2;
+            v2 = v1 - 10;
+
+            System.out.println("1번 영역의 v2 " + v2);
+        }
+        int v2 = 10;
+        int v3 = v1 + v2 + 5;
+
+        System.out.println("2번 영역의 v2 " + v2);
+        
+    }
+
+}
