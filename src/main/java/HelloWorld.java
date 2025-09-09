6a6523998a270eee0e69ef1a95931b9ec66314b7
danbihyun <sessak02@naver.com>
2025-09-09 15:07:17 +0900

    upddate

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,20 +3,31 @@
  * 작성일: 2025.09.09
  */
 
+// public class HelloWorld {
+//     public static void main(String[] args) {
+
+//         int v1 = 15;
+//         if (v1 > 10) {
+//             int v2;
+//             v2 = v1 - 10;
+
+//             System.out.println("1번 영역의 v2 : " + v2);
+//         }
+//         int v2 = 10;
+//         int v3 = v1 + v2 + 5;
+
+//         System.out.println("2번 영역의 v2 : " + v2);
+        
+//     }
+
+// }
+
 public class HelloWorld {
     public static void main(String[] args) {
 
-        int v1 = 15;
-        if (v1 > 10) {
-            int v2;
-            v2 = v1 - 10;
+        char var2 = 65;
 
-            System.out.println("1번 영역의 v2 " + v2);
-        }
-        int v2 = 10;
-        int v3 = v1 + v2 + 5;
-
-        System.out.println("2번 영역의 v2 " + v2);
+        System.out.println("1번 영역의 var2 : " + var2);
         
     }
 
