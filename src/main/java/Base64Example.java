5f36aeab514e6dda9a3736fc66d9334f6bf8a18c
danbihyun <sessak02@naver.com>
2025-09-10 11:23:29 +0900

    update

--- src/main/java/Base64Example.java
+++ src/main/java/Base64Example.java
@@ -2,15 +2,15 @@ import java.util.Base64;
 
 public class Base64Example {
     public static void main(String[] args) {
-        String plainText = "Hello Base64!";
+        String plainText = "Hello Base64 Encoding!";
 
-        // 인코딩 (텍스트 → Base64)
+        // 인코딩 (문자열 → Base64)
         String encoded = Base64.getEncoder().encodeToString(plainText.getBytes());
-        System.out.println("인코딩 결과: " + encoded);
+        System.out.println("Encoded : " + encoded);
 
-        // 디코딩 (Base64 → 텍스트)
+        // 디코딩 (Base64 → 문자열)
         byte[] decodedBytes = Base64.getDecoder().decode(encoded);
         String decoded = new String(decodedBytes);
-        System.out.println("디코딩 결과: " + decoded);
+        System.out.println("Decoded : " + decoded);
     }
 }
