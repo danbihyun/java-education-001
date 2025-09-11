906dea7096d3cad83f43ec982803cb8c46308268
danbihyun <sessak02@naver.com>
2025-09-11 09:21:55 +0900

    up

---
+++ src/main/java/Example1.java
@@ -0,0 +1,16 @@
+public class Example1 {
+    public static void main(String[] args) {
+        byte bytalVal = 10;
+        int intVal = (int) bytalVal;
+        System.out.println("intVal: " + intVal);
+
+        char charVal = '가';
+        intVal = charVal;
+        System.out.println("intVal: " + intVal);
+
+        int intVal1 = 500;
+        long longVal = intVal1;
+        System.out.println("longVal: " + longVal);
+
+    }
+}
